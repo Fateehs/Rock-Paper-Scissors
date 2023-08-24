9df58d06ae73e5764ac4416efe176ff732b64d98
Fateehs <benzer1010@gmail.com>
2023-08-24 18:58:36 +0300

    JS methods changed

--- js/rps.js
+++ js/rps.js
@@ -1,6 +1,7 @@
 // Select index.html properties which we need to use
 const DFCBTN = document.querySelector("#difficulty");
 const DFCS = document.querySelector("#difficultys");
+const DFCSBTN = document.querySelector("#difficultys button");
 
 // Want to be sure buttons are created once
 let buttonsCreated = false;
@@ -12,25 +13,27 @@ function selectDifficulty() {
 // Create difficulty buttons function
 const createDifficultyButtons = () => {
   if (!buttonsCreated) {
-    let normalDifficulty = document.createElement("button");
-    let hardcoreDifficulty = document.createElement("button");
+    let normalDifficulty = document.createElement("span");
+    let hardcoreDifficulty = document.createElement("span");
     normalDifficulty.classList.add("button");
     hardcoreDifficulty.classList.add("button", "hardcore");
     normalDifficulty.innerHTML = "Normal";
+    normalDifficulty.setAttribute("id", "normalbtn");
     hardcoreDifficulty.innerHTML = "Hardcore";
+    hardcoreDifficulty.setAttribute("id", "hardcorebtn");
     DFCS.append(normalDifficulty);
     DFCS.append(hardcoreDifficulty);
 
     buttonsCreated = true;
-    removeDifficultyButtons();
   }
 };
 
-const removeDifficultyButtons = () => {
-  document.querySelectorAll("#difficultys button").forEach((item) => {
-    item.addEventListener("click", removeElement(item));
+document.querySelectorAll("#difficultys button").forEach((item) => {
+  let btn = item.parentNode;
+  item.addEventListener("click", () => {
+    removeElement(btn);
   });
-};
+});
 
 // Function to remove element
 const removeElement = (element) => element.remove();
