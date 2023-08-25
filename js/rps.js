ae4167a23331ce447abd5a3e5518c6b902e26eaa
Fateehs <benzer1010@gmail.com>
2023-08-25 14:00:10 +0300

    Logic of JS modified

--- js/rps.js
+++ js/rps.js
@@ -1,39 +1,116 @@
-// Select index.html properties which we need to use
-const DFCBTN = document.querySelector("#difficulty");
-const DFCS = document.querySelector("#difficultys");
-const DFCSBTN = document.querySelector("#difficultys button");
+// Complete logic of game inside this function
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+  let moves = 0;
 
-// Want to be sure buttons are created once
-let buttonsCreated = false;
+  // Function to
+  const playGame = () => {
+    const rockBtn = document.querySelector(".rock");
+    const paperBtn = document.querySelector(".paper");
+    const scissorBtn = document.querySelector(".scissor");
+    const playerOptions = [rockBtn, paperBtn, scissorBtn];
+    const computerOptions = ["rock", "paper", "scissors"];
 
-function selectDifficulty() {
-  createDifficultyButtons();
-}
+    // Function to start playing game
+    playerOptions.forEach((option) => {
+      option.addEventListener("click", function () {
+        const movesLeft = document.querySelector(".movesleft");
+        moves++;
+        movesLeft.innerText = `Moves Left: ${10 - moves}`;
 
-// Create difficulty buttons function
-const createDifficultyButtons = () => {
-  if (!buttonsCreated) {
-    let normalDifficulty = document.createElement("span");
-    let hardcoreDifficulty = document.createElement("span");
-    normalDifficulty.classList.add("button");
-    hardcoreDifficulty.classList.add("button", "hardcore");
-    normalDifficulty.innerHTML = "Normal";
-    normalDifficulty.setAttribute("id", "normalbtn");
-    hardcoreDifficulty.innerHTML = "Hardcore";
-    hardcoreDifficulty.setAttribute("id", "hardcorebtn");
-    DFCS.append(normalDifficulty);
-    DFCS.append(hardcoreDifficulty);
+        const choiceNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[choiceNumber];
 
-    buttonsCreated = true;
-  }
+        // Function to check who wins
+        winner(this.innerText, computerChoice);
+
+        // Calling gameOver function after 10 moves
+        if (moves == 10) {
+          gameOver(playerOptions, movesLeft);
+        }
+      });
+    });
+  };
+
+  // Function to decide winner
+  const winner = (player, computer) => {
+    const result = document.querySelector(".result");
+    const playerScoreBoard = document.querySelector(".p-count");
+    const computerScoreBoard = document.querySelector(".c-count");
+    player = player.toLowerCase();
+    computer = computer.toLowerCase();
+    if (player === computer) {
+      result.textContent = "Tie";
+    } else if (player == "rock") {
+      if (computer == "paper") {
+        result.textContent = "Computer Won";
+        computerScore++;
+        computerScoreBoard.textContent = computerScore;
+      } else {
+        result.textContent = "Player Won";
+        playerScore++;
+        playerScoreBoard.textContent = playerScore;
+      }
+    } else if (player == "scissors") {
+      if (computer == "rock") {
+        result.textContent = "Computer Won";
+        computerScore++;
+        computerScoreBoard.textContent = computerScore;
+      } else {
+        result.textContent = "Player Won";
+        playerScore++;
+        playerScoreBoard.textContent = playerScore;
+      }
+    } else if (player == "paper") {
+      if (computer == "scissors") {
+        result.textContent = "Computer Won";
+        computerScore++;
+        computerScoreBoard.textContent = computerScore;
+      } else {
+        result.textContent = "Player Won";
+        playerScore++;
+        playerScoreBoard.textContent = playerScore;
+      }
+    }
+  };
+
+  // Function to run when game is over
+  const gameOver = (playerOptions, movesLeft) => {
+    const chooseMove = document.querySelector(".move");
+    const result = document.querySelector(".result");
+    const reloadBtn = document.querySelector(".reload");
+
+    playerOptions.forEach((option) => {
+      option.style.display = "none";
+    });
+
+    chooseMove.innerText = "Game Over!!";
+    movesLeft.style.display = "none";
+
+    if (playerScore > computerScore) {
+      result.style.fontSize = "2rem";
+      result.innerText = "You Won The Game";
+      result.style.color = "#308D46";
+    } else if (playerScore < computerScore) {
+      result.style.fontSize = "2rem";
+      result.innerText = "You Lost The Game";
+      result.style.color = "red";
+    } else {
+      result.style.fontSize = "2rem";
+      result.innerText = "Tie";
+      result.style.color = "grey";
+    }
+    reloadBtn.innerText = "Restart";
+    reloadBtn.style.display = "flex";
+    reloadBtn.addEventListener("click", () => {
+      window.location.reload();
+    });
+  };
+
+  // Calling playGame function inside game
+  playGame();
 };
 
-document.querySelectorAll("#difficultys button").forEach((item) => {
-  let btn = item.parentNode;
-  item.addEventListener("click", () => {
-    removeElement(btn);
-  });
-});
-
-// Function to remove element
-const removeElement = (element) => element.remove();
+// Calling the game function
+game();
